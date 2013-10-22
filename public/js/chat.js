0e514a421450dbc20c31610183aff3bc4e25f808
rajesh <rkjha.it.in@gmail.com>
2013-10-22 23:39:49 +0530

    one to one chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,28 +1,31 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://192.168.1.5:3700');
+    var socket = io.connect('http://192.168.1.2:3700');
     var field = document.getElementById("query-input");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("msg-container");
  
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += '<div class="bs-callout"><div class="avtar"><span class="glyphicon glyphicon-user"></span></div><div class="message-content">'+messages[i] + '</div></div>';
+            	html += '<div class="bs-callout"><div class="avtar"><span class="glyphicon glyphicon-user"></span></div><div class="message-content">';
+                html += '<h4>'+(messages[i].username ? messages[i].username : 'Server')+'</h4>';
+                html += messages[i].message + '</div></div>';
             }
             content.innerHTML = html;
         } else {
             console.log("There is a problem:", data);
         }
     });
+ 
     $('#selfsearch-form').on('submit',function(e){
 		e.preventDefault();
-		var text = field.value;
-        socket.emit('send', { message: text });
+       
+            var text = field.value;
+            socket.emit('send', { message: text, username: 'Me' });
     });
-   
  
 };
