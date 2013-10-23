da340c378c932c366d575c67cb06f805d824f665
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2013-10-23 09:59:30 +0530

    PORT Change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3700);
+app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
